e17f575c93ee8f86177981e21525943748fffbbb
vcokltfre <vcokltfre@gmail.com>
2020-11-13 04:05:27 +0000

    feat: add basic database support

---
+++ bot/cogs/utility/database.py
@@ -0,0 +1,33 @@
+import asyncio
+import aiomysql
+from aioredis.pubsub import Receiver
+from discord.ext import commands
+
+from bot.bot import Bot
+from bot.utils.checks import is_dev
+from config.config import maria
+
+
+class Database(commands.Cog):
+    """Database commands"""
+
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not hasattr(self.bot, "pool"):
+            self.pool = await aiomysql.create_pool(**maria, loop=asyncio.get_event_loop())
+
+    @commands.command(name="db")
+    @is_dev()
+    async def db(self, ctx: commands.Context, *, query: str):
+        async with self.pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(query)
+                (r,) = await cur.fetchone()
+                await ctx.send(r)
+
+
+def setup(bot: Bot):
+    bot.add_cog(Database(bot))
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from bot.bot import run
 
 run([
     "jishaku",
-    "bot.cogs.utility.general"
+    "bot.cogs.utility.general",
+    "bot.cogs.utility.database"
 ], debug=False)
