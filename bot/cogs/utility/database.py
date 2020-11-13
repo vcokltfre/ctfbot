5ba136600c46103b6e70d914b40d47118ec4dbb7
vcokltfre <vcokltfre@gmail.com>
2020-11-13 04:13:21 +0000

    fix: set bot attribute pool not cog attr

--- bot/cogs/utility/database.py
+++ bot/cogs/utility/database.py
@@ -17,12 +17,12 @@ class Database(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         if not hasattr(self.bot, "pool"):
-            self.pool = await aiomysql.create_pool(**maria, loop=asyncio.get_event_loop())
+            self.bot.pool = await aiomysql.create_pool(**maria, loop=asyncio.get_event_loop())
 
     @commands.command(name="db")
     @is_dev()
     async def db(self, ctx: commands.Context, *, query: str):
-        async with self.pool.acquire() as conn:
+        async with self.bot.pool.acquire() as conn:
             async with conn.cursor() as cur:
                 await cur.execute(query)
                 (r,) = await cur.fetchone()
